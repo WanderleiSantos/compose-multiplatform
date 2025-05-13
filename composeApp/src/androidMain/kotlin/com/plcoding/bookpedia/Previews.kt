77bb42325ef82a3c8723c8510f2e9de57dc01dd9
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2025-05-12 21:20:57 -0300

    create BookListScreen

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -8,6 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -24,4 +27,31 @@ private fun BookSearchBarPreview() {
             onImeSearch = {}
         )
     }
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Wanderlei Santos"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishedYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
